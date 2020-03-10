a1505bd93ed20d5fd54bb983abe894c99d9eff8b
rozo27s <aljarrome@hotmail.com>
2020-03-09 23:57:43 -0500

    Enviar email desde PHP

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php  
+
+// Llamando a los campos
+$correoDes = $_POST['correoDestinatario'];
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "$correoDes";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
+
+?>
